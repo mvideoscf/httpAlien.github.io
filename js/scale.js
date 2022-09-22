81dec154dafb0952a3e2f119d64d6a6cda18dda1
ᘉᓰᑢᖽᐸ <105106091+httpAlien@users.noreply.github.com>
2022-09-22 18:12:45 -0500

    Add files via upload

---
+++ js/scale.js
@@ -0,0 +1,6 @@
+if(screen.width >=2500)
+	document.write("<style> body {zoom:150%} </style>")
+if(screen.width <=1700)
+	document.write("<style> body {zoom:90%} </style>")
+if(screen.width <=1400)
+	document.write("<style> body {zoom:70%} </style>")
